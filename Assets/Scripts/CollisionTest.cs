6ab055e4d12448fac4eb1ae6cf97cee6b6ff7496
subtle852 <subtle852@gmail.com>
2025-06-30 23:32:14 +0900

    feat: Collision, Trigger 실습

---
+++ Assets/Scripts/CollisionTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTest : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"Collision is called by {collision.gameObject.name}");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Trigger is called by {other.gameObject.name}");
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
