b41f470bd5fc0689e665e4d1f904e25e43d6ec11
subtle852 <subtle852@gmail.com>
2025-07-05 04:46:21 +0900

    feat: KeyFrame Animation (Key, Event) 실습

---
+++ Assets/Scripts/Test/AnimationEventTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationEventTest : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void TestEvent()
+    {
+        Debug.Log($"Event is called");
+    }
+
+    public void TestEvent2(int a)
+    {
+        Debug.Log($"Event is called {a}");
+    }
+}
