75aa7b69bf2344093a990a99fd1269d8419ed6ce
subtle852 <subtle852@gmail.com>
2025-07-10 01:42:25 +0900

    feat: UI 자동화 - Bind, Get 구현

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InputManager
 {
@@ -11,6 +12,9 @@ public class InputManager
 
     public void OnUpdate()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+            return;
+
         if (KeyAction != null && Input.anyKey)
             KeyAction.Invoke();
 
