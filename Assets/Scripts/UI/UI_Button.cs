8567452a6a9b5fb6a94bb2dbe76f3ae8ad690525
subtle852 <subtle852@gmail.com>
2025-07-10 02:02:08 +0900

    feat: UI 자동화 - UI_Base 클래스 구현

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -4,10 +4,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
-    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>(); 
-
     enum Buttons
     {
         PointButton,
@@ -32,38 +30,6 @@ public class UI_Button : MonoBehaviour
 
         GetText((int)Texts.ScoreText).text = "Bind Test";
     }
-
-    void Bind<T>(Type type) where T : UnityEngine.Object
-    {
-        string[] names = Enum.GetNames(type);
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        _objects.Add(typeof(T), objects);
-
-        for (int i = 0; i < names.Length; i++)
-        {
-            if(typeof(T) == typeof(GameObject))
-                objects[i] = Util.FindChild(gameObject, names[i], true);
-            else
-                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
-
-            if (objects[i] == null)
-                Debug.Log($"Failed to bind ({names[i]})");
-        }
-
-    }
-
-    T Get<T>(int idx) where T : UnityEngine.Object
-    {
-        UnityEngine.Object[] objects = null;
-        if (_objects.TryGetValue(typeof(T), out objects) == false)
-            return null;
-
-        return objects[idx] as T;
-    }
-
-    Text GetText(int idx) { return Get<Text>(idx); }
-    Button GetButton(int idx) { return Get<Button>(idx); }
-    Image GetImage(int idx) { return Get<Image>(idx); }
 
     int _score = 0;
 
