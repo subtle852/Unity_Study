cf80347f176fdc214bd291e3b29b0b0bd7fdaafc
subtle852 <subtle852@gmail.com>
2025-07-16 01:32:17 +0900

    feat: SoundManager 구현

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,13 +4,63 @@ using UnityEngine;
 
 public class SoundManager
 {
-    void Start()
+    AudioSource[] _audioSources = new AudioSource[(int)Define.Sound.MaxCount];
+
+    public void Init()
     {
-        
+        GameObject root = GameObject.Find("@Sound");
+        if (root == null)
+        {
+            root = new GameObject { name = "@Sound" };
+            Object.DontDestroyOnLoad(root);
+
+            string[] soundNames = System.Enum.GetNames(typeof(Define.Sound));
+            for (int i = 0; i < soundNames.Length - 1; i++)
+            {
+                GameObject go = new GameObject { name = soundNames[i] };
+                _audioSources[i] = go.AddComponent<AudioSource>();
+                go.transform.parent = root.transform;
+            }
+
+            _audioSources[(int)Define.Sound.Bgm].loop = true;
+        }
     }
 
-    void Update()
+    public void Play(string path, Define.Sound type = Define.Sound.Effect, float pitch = 1.0f)
     {
-        
+        if (path.Contains("Sounds/") == false)
+            path = $"Sounds/{path}";
+
+        if (type == Define.Sound.Bgm)
+        {
+            AudioClip audioClip = Managers.Resource.Load<AudioClip>(path);
+            if (audioClip == null)
+            {
+                Debug.Log($"AudioClip is missing! {path}");
+                return;
+            }
+
+            AudioSource audioSource = _audioSources[(int)Define.Sound.Bgm];
+            if (audioSource.isPlaying)
+                audioSource.Stop();
+
+            audioSource.pitch = pitch;
+            audioSource.clip = audioClip;
+            audioSource.Play();
+        }
+        else
+        {
+            AudioClip audioClip = Managers.Resource.Load<AudioClip>(path);
+            if (audioClip == null)
+            {
+                Debug.Log($"AudioClip is missing! {path}");
+                return;
+            }
+
+            AudioSource audioSource = _audioSources[(int)Define.Sound.Effect];
+            audioSource.pitch = pitch;
+            audioSource.PlayOneShot(audioClip);
+        }
+
     }
 }
--- Assets/Scripts/Test/SoundTest.cs
+++ Assets/Scripts/Test/SoundTest.cs
@@ -16,13 +16,29 @@ public class SoundTest : MonoBehaviour
 
     public AudioClip audioClip;
     public AudioClip audioClip2;
+
+    int i = 0;
     private void OnTriggerEnter(Collider other)
     {
-        AudioSource audio = GetComponent<AudioSource>();
-        audio.PlayOneShot(audioClip);
-        audio.PlayOneShot(audioClip2);
+        // 기본 실습
+        //AudioSource audio = GetComponent<AudioSource>();
+        //audio.PlayOneShot(audioClip);
+        //audio.PlayOneShot(audioClip2);
 
-        float lifeTime = Mathf.Max(audioClip.length, audioClip2.length);
-        GameObject.Destroy(gameObject, lifeTime);
+        //float lifeTime = Mathf.Max(audioClip.length, audioClip2.length);
+        //GameObject.Destroy(gameObject, lifeTime);
+
+        // SoundManager Effect 실습
+        //Managers.Sound.Play("UnityChan/univ0001");
+        //Managers.Sound.Play("UnityChan/univ0002");
+
+        // SoundManager Bgm 실습
+        i++;
+
+        if(i % 2 == 0 )
+            Managers.Sound.Play("UnityChan/univ0001", Define.Sound.Bgm);
+        else
+            Managers.Sound.Play("UnityChan/univ0002", Define.Sound.Bgm);
+
     }
 }
