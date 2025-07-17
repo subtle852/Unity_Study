228de77425d6d184477058d1348b497d9f0d2508
subtle852 <subtle852@gmail.com>
2025-07-17 16:43:28 +0900

    feat: PoolManager 구현

--- Assets/Scripts/Managers/PoolManager.cs
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class PoolManager
 {
@@ -71,20 +72,51 @@ public class PoolManager
         }
     }
 
+    public void CreatePool(GameObject original, int count = 5)
+    { 
+        Pool pool = new Pool();
+        pool.Init(original, count);
+        pool.Root.parent = _root;
+
+        _pool.Add(original.name, pool);
+    }
+
     public void Push(Poolable poolable)
     {
+        string name = poolable.gameObject.name;
+        if (_pool.ContainsKey(name) == false)
+        {
+            // 툴에서 드래그 드랍으로 생성한 경우에는
+            // Pool이 없는 상태에서 Push를 할 수 있음
 
+            GameObject.Destroy(poolable.gameObject);
+            return;
+        }
+
+        _pool[name].Push(poolable);
     }
 
     public Poolable Pop(GameObject original, Transform parent = null)
     {
-        return null;
+        if (_pool.ContainsKey(original.name) == false)
+            CreatePool(original);
+
+        return _pool[original.name].Pop(parent);
     }
 
     public GameObject GetOriginal(string name)
     {
-        return null;
+        if (_pool.ContainsKey(name) == false)
+            return null;
+
+        return _pool[name].Original;
     }
 
+    public void Clear()
+    {
+        foreach (Transform child in _root)
+            GameObject.Destroy(child.gameObject);
 
+        _pool.Clear();
+    }
 }
