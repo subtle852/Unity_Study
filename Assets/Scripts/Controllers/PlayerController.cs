e52c374424c54e233865dd851b96c8bca4968b2b
subtle852 <subtle852@gmail.com>
2025-07-22 23:47:12 +0900

    feat: 이동 코드 구현

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -46,15 +47,25 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 dir = _mouseMoveDestPos - transform.position;
         dir.y = 0.0f;
-        if (dir.magnitude < 0.0001f)
+        if (dir.magnitude < 0.01f)
         {
             _state = PlayerState.Idle;
         }
         else
         {
+            NavMeshAgent nma = gameObject.GetOrAddComponent<NavMeshAgent>();
+            
             float moveDist = Mathf.Clamp(_positionSpeed * Time.deltaTime, 0, dir.magnitude);
+            //transform.position += dir.normalized * moveDist;
+            nma.Move(dir.normalized * moveDist);
 
-            transform.position += dir.normalized * moveDist;
+            Debug.DrawRay(transform.position + Vector3.up * 0.5f, dir.normalized, Color.green);
+            if(Physics.Raycast(transform.position + Vector3.up * 0.5f, dir, 1.0f, LayerMask.GetMask("Block")))
+            {
+                _state = PlayerState.Idle;
+                return;
+            }
+
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
         }
 
